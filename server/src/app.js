e1244192c49e54ded6c2779da90676d3a982856f
hmad213 <142343785+hmad213@users.noreply.github.com>
2026-04-17 20:12:15 +0500

    mend

--- server/src/app.js
+++ server/src/app.js
@@ -1,12 +1,9 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
-const queries = require("./db/customer_queries");
 
 app.get("/", (req, res) => {
   res.send("Hello world");
-
-  queries.getCustomer(1);
 });
 
 app.listen(PORT, (error) => {
